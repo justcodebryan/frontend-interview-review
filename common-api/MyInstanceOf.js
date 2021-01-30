4b31c5efcd20bf7e47ed3bd0c0a9206afb28e2f8
Bryan <Huangjifan1819@163.com>
2021-01-30 20:17:12 +0800

    feat:20210130复习

--- common-api/MyInstanceOf.js
+++ common-api/MyInstanceOf.js
@@ -1,2 +1,21 @@
 // MyInstanceOf
 // TODO
+function MyInstanceOf(L, R) {
+  const proto = L.__proto__;
+  while (R) {
+    if (proto === R.prototype) return true;
+    R = R.prototype;
+  }
+  return false;
+}
+
+function Creator(age) {
+  this.age = age;
+}
+
+const obj = [];
+const obj1 = new Creator(10);
+console.log("obj -> Array", obj instanceof Array);
+console.log("obj -> Creator", MyInstanceOf(obj, Creator));
+console.log("obj1 -> Creator", MyInstanceOf(obj1, Creator));
+console.log("obj1 -> Creator", obj1 instanceof Creator);

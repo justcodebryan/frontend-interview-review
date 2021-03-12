3478cecc8778d9ade97dc1368edb4b6fb1556e0a
gcodeknight <44990896+gcodeknight@users.noreply.github.com>
2021-03-12 15:45:42 +0800

    Update binaryTreeBFSTemp.js

--- algroithm/algorithm-template/binaryTreeBFSTemp.js
+++ algroithm/algorithm-template/binaryTreeBFSTemp.js
@@ -3,7 +3,19 @@ const binaryTreeBFS = function (root) {
 
   if (root === null) return res;
 
+  const res = [];
   const queue = [];
   queue.push(root);
-
+  while (queue.length) {
+    let size = queue.length;
+    const level = [];
+    while (size--) {
+      let node = queue.pop();
+      level.push(node);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    res.push(level);
+  }
+  return res;
 }

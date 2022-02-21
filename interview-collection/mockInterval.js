ef557dfd8d5bd1f1fda10a1f52251acf5fc3fcf4
gcodeknight <44990896+gcodeknight@users.noreply.github.com>
2022-02-21 17:22:36 +0800

    feat: 20220221

---
+++ interview-collection/mockInterval.js
@@ -0,0 +1,14 @@
+function mockRequest() {
+  return new Promise((resolve) => {
+    globalThis.setTimeout(() => {
+      console.log(Date.now());
+      resolve();
+    }, Math.ceil(Math.random() * 1000000));
+  });
+}
+
+function mockInterval(func, interval) {
+  return Promise.all([func().catch(() => void 0), new Promise(resolve => setTimeout(resolve, interval))]).then(() => mockInterval(func, interval));
+}
+
+mockInterval(mockRequest, 3000);
